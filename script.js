92cb811177327989087f5553312626e7a9ea9581
Loudjair Mauvais <mr.loudjair@gmail.com>
2021-04-22 20:03:22 +0200

    Initialisation projet

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var currentSection = "viande";
+function affiche(id){
+	
+	if(id != currentSection){
+		//Cacher l'ancienne section courante
+		document.getElementById(currentSection).classList.toggle("currentSection");
+		document.getElementById(currentSection).classList.toggle("ignoreSection");
+		
+		//Afficher la nouvelle section courante
+		currentSection = id;
+		document.getElementById(id).classList.toggle("ignoreSection");
+		document.getElementById(id).classList.toggle("currentSection");
+
+		//Update du placeholder de la barre de recherche
+		document.getElementById("barreSearch").placeholder = "Recherchez "+ ((id == "viande") ? "une " : "un ") + id;
+	}
+}
